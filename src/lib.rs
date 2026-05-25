86ec6359258e57a8af391fafc645d6ffab363bff
Scott McMurray <scottmcm@users.noreply.github.com>
2026-05-25 11:05:04 -0700

    Use `ControlFlow` in `fold_while` implementation

--- src/lib.rs
+++ src/lib.rs
@@ -3169,7 +3169,7 @@ pub trait Itertools: Iterator {
         Self: Sized,
         F: FnMut(B, Self::Item) -> FoldWhile<B>,
     {
-        use Result::{Err as Break, Ok as Continue};
+        use core::ops::ControlFlow::{Break, Continue};
 
         let result = self.try_fold(
             init,
